57a092cb929c8be5aa2193b07b1b3e4a0d501dda
Jérémie Zarca <jeremie.zarca@gmail.com>
2017-12-25 17:56:31 +0100

    initial poc

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pocHotdog
+
+Uses your iDevice to determine if the output of your camera is a hotdog or not
+
+## Requirements
+
+- iOS 11
+- Xcode 9+
---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  pocHotdog
+//
+//  Created by Jérémie Zarca on 24/12/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+import Foundation
